5bad74a44a05bb57df41e9bb504e8f497220ade2
Gabriel Chaves <gabrieloliveirachaves754@gmail.com>
2021-12-16 13:23:55 +0000

    mock data and first route

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 const http = require('http');
-
+const users = require('./mocks/users');
 const server = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'html/text' });
-  response.end('<h1>Olá mundo</h1>');
-});
+  console.log(`Request method: ${request.method} | Endpoint: ${request.url}`)
 
+  if (request.url === '/users' && request.method === 'GET') {
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    response.end(JSON.stringify(users));
+  } else {
+    response.writeHead(404, { 'Content-Type': 'text/html' });
+    response.end(`Cannot ${request.method} ${request.url}`)
+  }
+});
 server.listen(3000, () => console.log('server is up and running at http://localhost:3000'));
---
+++ src/mocks/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'goku'
+  },
+  {
+    id: 2,
+    name: 'vegeta'
+  },
+  {
+    id: 3,
+    name: 'gohan'
+  },
+]
