2f59514da736a9a60c2fca17c5565bc297b42af9
Gabriel Chaves <gabrieloliveirachaves754@gmail.com>
2021-12-17 17:54:34 +0000

    PUT and DELETE routes

--- src/routes.js
+++ src/routes.js
@@ -15,4 +15,14 @@ module.exports = [
     method: 'POST',
     handler: UserControler.createUser,
   },
+  {
+    endpoint: '/users/:id',
+    method: 'PUT',
+    handler: UserControler.updateUser,
+  },
+  {
+    endpoint: '/users/:id',
+    method: 'DELETE',
+    handler: UserControler.deleteUser,
+  },
 ]
